121b8f53ea20a5b218840c1dac12f38288504828
Jonathan Kana <jokana2008@gmail.com>
2026-08-15 15:21:52 +0000

    Add File Submission Template

--- Week 2/Lab2.py
+++ Week 2/Lab2.py
@@ -1,13 +1,82 @@
-kumpulan_integer = [2,0,2,6]
-sebuah_string = "Dekdepe"
+# Header
+print("============================================================")
+print("SISTEM PENCATATAN ANGGOTA KDMP FLORIAN")
+print("============================================================")
 
-list_satu = list()
-list_dua = list(kumpulan_integer)
-list_tiga = list(sebuah_string)
 
-print(list_satu)
-print(type(list_satu))
-print(list_dua)
-print(type(list_dua))
-print(list_tiga)
-print(type(list_tiga))
+def tentukan_paket(skor_usaha):
+    # TODO: Menentukan paket berdasarkan skor usaha
+    if ...:
+        return "Paket Tunas"
+    elif ...:
+        return "Paket Mekar"
+    else:
+        return "Paket Mandiri"
+
+
+# TODO: Meminta banyak anggota yang akan didaftarkan
+daftar_anggota = []
+banyak_anggota = ...
+
+# TODO: Meminta data untuk setiap anggota
+for nomor in range(..., ...):
+    print(f"\n=== Data anggota {nomor} ===")
+    # TODO: Mengambil input
+    nama = ...
+    kode_wilayah = ...
+    skor_usaha = ...
+    komoditas = ...
+
+    # TODO: Mengubah kode wilayah menjadi nama wilayah
+    if ...:
+        wilayah = "Pegunungan"
+    elif ...:
+        wilayah = "Lembah"
+    else:
+        wilayah = "Pesisir"
+
+    paket = tentukan_paket(...)
+
+    # Data anggota disimpan sebagai tuple dengan urutan:
+    # Nama, Wilayah, Skor Usaha, Komoditas, Paket
+    data_anggota = (..., ..., ..., ..., ...)
+    daftar_anggota.append(...)
+
+
+# TODO: Menampilkan data anggota dalam bentuk tabel
+jumlah_tunas = 0
+jumlah_mekar = 0
+jumlah_mandiri = 0
+
+print("\n====================== DATA ANGGOTA KDMP ======================")
+print(f"{'No':<3} {'Nama':<12} {'Wilayah':<11} {'Skor':>4} {'Komoditas':<12} {'Paket':<14}")
+
+for i in range(...):
+    anggota = daftar_anggota[...]
+    nama, wilayah, skor_usaha, komoditas, paket = ...
+
+    print(f"{...:<3} {nama:<12} {...:<11} {skor_usaha:>4} {...:<12} {paket:<14}")
+
+    # TODO: Menghitung jumlah anggota pada setiap paket
+    if ...:
+        jumlah_tunas += 1
+    elif ...:
+        jumlah_mekar += 1
+    else:
+        jumlah_mandiri += 1
+
+
+print("====================== RINGKASAN KDMP ======================")
+print(f"Jumlah penerima Paket Tunas: {...}")
+print(f"Jumlah penerima Paket Mekar: {...}")
+print(f"Jumlah penerima Paket Mandiri: {...}")
+
+# BONUS: uncomment dan lengkapi jika mengerjakan bonus
+# def tampilkan_anggota_per_paket(daftar_anggota):
+#     ...
+#
+# tampilkan_anggota_per_paket(daftar_anggota)
+
+print("============================================================")
+print("TERIMA KASIH SUDAH MEMBANTU KDMP FLORIAN!")
+print("============================================================")
