c2d888c6f9fc54a08e4c7d7911e5de79906fcb00
Jonathan Kana <jokana2008@gmail.com>
2026-08-15 14:20:22 +0000

    Example

---
+++ Week 2/Lab2.py
@@ -0,0 +1,13 @@
+kumpulan_integer = [2,0,2,6]
+sebuah_string = "Dekdepe"
+
+list_satu = list()
+list_dua = list(kumpulan_integer)
+list_tiga = list(sebuah_string)
+
+print(list_satu)
+print(type(list_satu))
+print(list_dua)
+print(type(list_dua))
+print(list_tiga)
+print(type(list_tiga))
